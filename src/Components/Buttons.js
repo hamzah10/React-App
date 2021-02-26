9eb5e097726078f5d880995a9e80bb2b6bec203f
Hamza Os <oshamza871@gmail.com>
2021-02-26 11:03:00 +0100

    first commit

---
+++ src/Components/Buttons.js
@@ -0,0 +1,28 @@
+import './Buttons.css';
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary','btn--outline'];
+const SIZES = ["btn--medium","btn--large"];
+export const Button = ({
+    children,
+    type,
+    onclick,
+    buttonStyle,
+    buttonSize,
+}) => {
+const checkButtonStyle = STYLES.includes(buttonStyle) 
+? buttonStyle 
+: STYLES[0];
+const checkButtonSize = SIZES.includes(buttonSize) 
+? buttonSize
+:SIZES[0];
+
+return ( 
+<Link to="/sign-up" className="btn-mobile">
+<Button className={'btn ${checkButtonStyle} ${CheckButtonSize}'} onclick={onclick}
+type={type}>
+    {children}
+</Button>
+</Link>
+)
+};
